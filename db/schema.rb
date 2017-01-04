b6b9e71bf7413b0d717462a3eaf77d329c7b34aa
Nick Brown <nickbrown714@gmail.com>
2017-01-04 15:44:51 -0800

    added student model for course roster

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170104225224) do
+ActiveRecord::Schema.define(version: 20170104234341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 20170104225224) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["thing_type", "thing_id", "var"], name: "index_settings_on_thing_type_and_thing_id_and_var", unique: true, using: :btree
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string   "perm"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
---
+++ spec/factories/students.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :student do
+    perm "MyString"
+    first_name "MyString"
+    last_name "MyString"
+    email "MyString"
+  end
+end
